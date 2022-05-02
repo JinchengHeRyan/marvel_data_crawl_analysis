e5cd4a5a38c53bda9752045e4d642c8045b3e206
JinchengHeRyan <delteforce@outlook.com>
2022-05-02 23:48:11 +0800

    Add download command into main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from crawlers.charactersCrawler import charactersCrawler
+from crawlers.imageDownloader import imageDownloader
 
 
 PUBLIC_KEY = "2c69de0f12c7f9d6406952e6aca6fab7"
@@ -12,4 +13,13 @@ chCrawler = charactersCrawler(
     output_dir="./data/characters/",
 )
 
-chCrawler.get_data()
+imageD = imageDownloader(
+    inputJsonDir="./data/characters", outputImageDir="./data/images/characters/"
+)
+
+if __name__ == "__main__":
+    # Get all characters and stored in json files
+    chCrawler.get_data()
+
+    # Download character images
+    imageD.downloadAll()
