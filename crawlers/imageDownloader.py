dc346c78490b7e6b9d499ccd9a799ad33143c6bf
JinchengHeRyan <delteforce@outlook.com>
2022-05-02 23:42:33 +0800

    Download method in imageDownloader complete

--- crawlers/imageDownloader.py
+++ crawlers/imageDownloader.py
@@ -1,6 +1,7 @@
 import os
 import json
 from typing import List
+import requests
 
 
 class imageDownloader:
@@ -25,11 +26,24 @@ class imageDownloader:
             data = json.load(file)
             assert data["code"] == 200
             for i in range(len(data["data"]["results"])):
-                ID2ImageURL[data["data"]["results"][i]["id"]] = "{}.{}".format(
+                ID2ImageURL[
+                    "{}.{}".format(
+                        data["data"]["results"][i]["id"],
+                        data["data"]["results"][i]["thumbnail"]["extension"],
+                    )
+                ] = "{}.{}".format(
                     data["data"]["results"][i]["thumbnail"]["path"],
                     data["data"]["results"][i]["thumbnail"]["extension"],
                 )
         return ID2ImageURL
 
     def downloadAll(self):
-        pass
+        ID2ImageURL = self.getIDImageURL()
+        os.makedirs(self.outputImageDir, exist_ok=True)
+        for file_name in ID2ImageURL.keys():
+            url = ID2ImageURL[file_name]
+            response = requests.get(url)
+            open(os.path.join(self.outputImageDir, file_name), "wb").write(
+                response.content
+            )
+        print("Download finished!")
