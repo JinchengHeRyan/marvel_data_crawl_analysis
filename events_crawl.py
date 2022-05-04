028c72f60b709040a1dca7811a83d86593497b2f
JinchengHeRyan <delteforce@outlook.com>
2022-05-04 20:20:28 +0800

    Add events crawl script

---
+++ events_crawl.py
@@ -0,0 +1,19 @@
+from crawlers.eventsCrawler import eventsCrawler
+
+
+PUBLIC_KEY = "2c69de0f12c7f9d6406952e6aca6fab7"
+PRIVATE_KEY = "1e7024cd196e873669e4782d9fa43db6bd181e9f"
+
+evCrawler = eventsCrawler(
+    PUBLIC_KEY=PUBLIC_KEY,
+    PRIVATE_KEY=PRIVATE_KEY,
+    num_total=20,
+    limit=50,
+    output_dir="./data/events/JSON/",
+    need_all_available=True,
+)
+
+
+if __name__ == "__main__":
+    # Get all events and stored in json files
+    evCrawler.get_data()
