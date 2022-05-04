abd558c1d735227365c1fe1ce8bf9e24f37d82ae
JinchengHeRyan <delteforce@outlook.com>
2022-05-04 20:47:48 +0800

    Add combine json function into utils

--- crawlers/utils/util.py
+++ crawlers/utils/util.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 import os
 
@@ -9,3 +10,20 @@ def getJsonFiles(inputJsonDir) -> List[str]:
         if "json" in file
     ]
     return files
+
+
+def combine_jsonFiles(inputJsonDir, outputCombinedDir):
+    combined = {"results": list()}
+    files = getJsonFiles(inputJsonDir=inputJsonDir)
+    for file_path in files:
+        file = open(file_path, "r")
+        data = json.load(file)
+        combined["results"] += data["data"]["results"]
+        file.close()
+
+    os.makedirs(outputCombinedDir, exist_ok=True)
+    output_path = os.path.join(outputCombinedDir, "combined.json")
+    outputFile = open(output_path, "w")
+    combined = json.dumps(combined, indent=4)
+    outputFile.write(combined)
+    outputFile.close()
