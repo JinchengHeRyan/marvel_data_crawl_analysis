c122ddc996abc66374106b996cc894efdba222ff
JinchengHeRyan <delteforce@outlook.com>
2022-05-05 05:01:13 +0800

    Add centrality to csv feature

--- centrality_compute.py
+++ centrality_compute.py
@@ -1,4 +1,3 @@
-import networkx as nx
 from graphBuilder.graph_build import GraphBuilder
 
 csv_path = "./data/events/CSV/events_characters/event2characters.csv"
@@ -7,8 +6,7 @@ graph_builder = GraphBuilder(csv_path=csv_path)
 
 
 if __name__ == "__main__":
-    graph = graph_builder.get_graph()
-    centrality = nx.betweenness_centrality(graph)
-
-    centrality = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
-    print(centrality)
+    graph_builder.centralityRank_to_csv(
+        characters_combined_json_path="./data/characters/JSON/combined/combined.json",
+        output_path="./data/characters/centrality/",
+    )
--- graphBuilder/graph_build.py
+++ graphBuilder/graph_build.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import pandas as pd
+import json
+import os
 
 
 class GraphBuilder:
@@ -32,3 +34,28 @@ class GraphBuilder:
             characters = self.data[eventID]
             self.build_connection(node_list=characters)
         return self.graph
+
+    def centralityRank_to_csv(
+        self, characters_combined_json_path: str, output_path: str
+    ):
+        json_file = open(characters_combined_json_path, "r")
+        data = json.load(json_file)
+        characters_dict = dict()
+        for i in range(len(data["results"])):
+            id = data["results"][i]["id"]
+            name = data["results"][i]["name"]
+            characters_dict[id] = name
+
+        self.get_graph()
+        centrality = nx.betweenness_centrality(self.graph)
+        centrality = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
+
+        os.makedirs(output_path, exist_ok=True)
+        csv_path = os.path.join(output_path, "centrality_rank.csv")
+        csv_file = open(csv_path, "w")
+        csv_file.write("id,name,score\n")
+        for item in centrality:
+            id = int(item[0])
+            score = item[1]
+            name = characters_dict[id]
+            csv_file.write("{},{},{}\n".format(str(id), name, str(score)))
