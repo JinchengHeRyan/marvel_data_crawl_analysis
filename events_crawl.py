68ebe88908d9a5587bf78b21d399c6e971d80f01
JinchengHeRyan <delteforce@outlook.com>
2022-05-05 02:11:40 +0800

    Add events crawling script

--- events_crawl.py
+++ events_crawl.py
@@ -1,5 +1,6 @@
 from crawlers.eventsCrawler import eventsCrawler
 from crawlers.utils.util import combine_jsonFiles
+from crawlers.events2charactersCrawler import events2charactersCrawler
 
 
 PUBLIC_KEY = "2c69de0f12c7f9d6406952e6aca6fab7"
@@ -14,6 +15,14 @@ evCrawler = eventsCrawler(
     need_all_available=True,
 )
 
+ev2ChCrawler = events2charactersCrawler(
+    PUBLIC_KEY=PUBLIC_KEY,
+    PRIVATE_KEY=PRIVATE_KEY,
+    limit=50,
+    combinedJsonDir="./data/events/JSON/combined/combined.json",
+    csvOutputPath="./data/events/CSV/events_characters/",
+)
+
 
 if __name__ == "__main__":
     # Get all events and stored in json files
@@ -24,3 +33,6 @@ if __name__ == "__main__":
         inputJsonDir="./data/events/JSON/",
         outputCombinedDir="./data/events/JSON/combined/",
     )
+
+    # Get events to characters
+    ev2ChCrawler.get_write_data()
