050b286b388d17776a663085fb0b3d73156662d4
JinchengHeRyan <delteforce@outlook.com>
2022-05-04 21:52:08 +0800

    Add getCharacters func for events2characters Crawler class

--- crawlers/events2charactersCrawler.py
+++ crawlers/events2charactersCrawler.py
@@ -15,6 +15,7 @@ class events2charactersCrawler(CrawlerBase):
         self.combinedJsonDir = combinedJsonDir
         self.csvOutputPath = csvOutputPath
         self.events_list = None
+        self.events = self.m.events
 
     def getEventsList(self):
         file = open(self.combinedJsonDir, "r")
@@ -24,3 +25,21 @@ class events2charactersCrawler(CrawlerBase):
         ]
         file.close()
         return self.events_list
+
+    def getCharacters(self, eventID: int):
+        """
+        Get characters for only one event
+        :param eventID: eventID
+        :return: list of characters for this certain event
+        """
+        offset = 0
+        characters = list()
+        total_num = self.events.characters(eventID)["data"]["total"]
+        for i in range(total_num // self.limit + 1):
+            data = self.events.characters(eventID, offset=offset)
+            characters += [
+                data["data"]["results"][_]["id"]
+                for _ in range(len(data["data"]["results"]))
+            ]
+            offset += self.limit
+        return characters
